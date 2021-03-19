fd3e66bb6aa92f05b5da5a7566f7153326b7a5f3
sgupt245 <80969981+sgupt245@users.noreply.github.com>
2021-03-19 16:05:42 -0400

    Add files via upload

---
+++ MSE2202-Gupta-251073715.ino
@@ -0,0 +1,28 @@
+//defining all base components
+int lPin1 = 34; //red led
+int lPin2 = 36; // yellow led
+int lPin3 = 32; // green led
+
+
+int servoPin = 14; //servoPin
+int servoChannel = 8; //using an open channel for servo
+int servoSpeed; // max/min speed for servo (ex: from 0 to 255)
+
+int switchPin = 33; //power switch
+int machineStatus;   //on/off status of treadmill
+
+
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
